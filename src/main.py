bcb0303f3ba88896698e4de09b553c8ba180d115
DavidCambra18 <cambrafontan.david@gmail.com>
2025-12-04 12:07:50 +0100

    init

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def elevar(base, exp):
+    """
+    Docstring para elevar
+    
+    :param base: Descripción
+    :param exp: Descripción
+    """
+    return base**exp
---
+++ test/test_main.py
@@ -0,0 +1,5 @@
+from src.main import elevar
+
+
+def test_elevar():
+    assert elevar(2, 3) == 8
